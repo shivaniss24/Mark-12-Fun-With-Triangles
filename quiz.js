1c8122fd363b0edb4c63e72428c80daaa01e975e
Shivani Soni <eng.shivanisoni2405@gmail.com>
2022-09-01 22:51:55 +0530

    triangle quiz complete

---
+++ quiz.js
@@ -0,0 +1,24 @@
+const quizForm=document.querySelector(".quiz-form");
+const submitBtn=document.querySelector("#submit-answer");
+const outputElement=document.querySelector("#output");
+const correctAnswers=["90°","right angled"];
+
+function calculateScore()
+{
+    let  score=0;
+    let index=0;
+    const formResults=new FormData(quizForm);
+    for(let value of formResults.values())
+    {
+        if(value===correctAnswers[index])
+        {
+            score=score + 1;
+        }
+        index=index+1;
+    }
+    // console.log(score);
+    outputElement.innerText="your score is " + score;
+
+}
+
+submitBtn.addEventListener("click",calculateScore);
